5857f35c30ed368eef27036f1577b8821247abe8
Jorge-Bot-bit <jor.reyesm@duocuc.cl>
2026-05-15 21:04:02 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,11 +1,11 @@
-function sumar(a, b) {
-  return a + b;
-}
+const sumar = (a, b) => a + b;
+
+console.log("Ejecutando pruebas unitarias...");
 
 if (sumar(2, 2) === 4) {
-  console.log(" :) Prueba unitaria exitosa: 2+2 es 4");
-  process.exit(0); 
+    console.log("✅ Prueba exitosa: 2 + 2 es igual a 4");
+    process.exit(0); 
 } else {
-  console.log(" :( Prueba fallida");
-  process.exit(1); /
+    console.log("❌ Prueba fallida");
+    process.exit(1);
 }
